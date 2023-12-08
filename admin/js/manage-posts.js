2e13c17a4dfc631e22377fab8dae01f42754c84b
Kenatho <89846381+Kenatho@users.noreply.github.com>
2023-12-08 22:50:47 +0100

    Fetched the tags and author

--- admin/js/manage-posts.js
+++ admin/js/manage-posts.js
@@ -1,48 +1,86 @@
-fetchAllPosts();
+fetchAllPuns();
 
-async function fetchAllPosts() {
+async function fetchAllPuns() {
     try {
         const response = await fetch('https://blog-api-assignment.up.railway.app/posts');
-        const posts = await response.json();
-        console.log(posts)
+        const puns = await response.json();
 
-        let postsListHTML = "";
-        for (let post of posts) {
-            let postDate = new Date(post.date)
+        let punsListHTML = "";
+        for (let pun of puns) {
+            let punDate = new Date(pun.date);
 
-            postsListHTML += `
-                <li class="list-group-item">
-                    <p>${post.content} <br> <span class="date">- ${postsDate.getFullYear()}-${postDate.getMonth()+1}-${postDate.getDate()} ${postDate.toLocaleTimeString()}</span> </p>
-                    
-                    <div>
-                        <a href="update-pun.html?id=${post._id}">Update</a> |
-                        <a href="#" data-id="${post._id}" class="delete-links">Delete</a> 
-                    </div>
-                </li>
-            `
+            punsListHTML += `
+                <tr data-id="${pun._id}">
+                    <td class="editable" contenteditable="true">${pun.title}</td>
+
+                    <td>${pun.author}</td>
+                    <td>${pun.content}</td>
+                    <td>${pun.tags}</td>
+                    <td>${punDate.toLocaleDateString()}</td>
+                    <td>
+                        <button class="update-btn">Update</button> |
+                        <button class="delete-btn" data-id="${pun._id}">Delete</button>
+                    </td>
+                </tr>
+            `;
+        }
+        document.getElementById('pun-list-body').innerHTML = punsListHTML;
+
+       /* This fetches the delete button and then adds an event listener to the 
+         delete button to make it functional */
+        let deleteBtns = document.querySelectorAll('.delete-btn');
+        for (let btn of deleteBtns) {
+            btn.addEventListener('click', async function (e) {
+                e.preventDefault();
+                const punIdToDelete = e.target.dataset.id;
+                await deletePun(punIdToDelete);
+            });
         }
 
-        document.getElementById('post-list').innerHTML = postsListHTML;
-    } catch(error) {
-        console.log(error)
-    }   
-    const deleteLinks = document.getElementsByClassName('delete-links');
-    console.log(deleteLinks)
-
-    for (let link of deleteLinks) {
-        link.addEventListener('click', async function(e) {
-            if (e.target.classname === 'delete-link') {
-
-            }
-            e.preventDefault();
-            let postId = e.target.dataset.id;
-            let response = await fetch(' https://blog-api-assignment.up.railway.app/posts/' + postId, {
-                method: `DELETE`
+        /* This fetches the update button and then adds an event listener to the 
+         update button to make it functional */
+        let updateBtns = document.querySelectorAll('.update-btn');
+        for (let btn of updateBtns) {
+            btn.addEventListener('click', async function (e) {
+                e.preventDefault();
+                const punIdToUpdate = e.target.closest('tr').dataset.id;
+                const newFirstName = e.target.closest('tr').querySelector('.editable').textContent;
+                await updatePun(punIdToUpdate, { title: newFirstName });
             });
-            
-            if(response.ok) {
-                e.target.parentNode.parentNode.remove();
-            }
-        })
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function deletePun(punId) {
+    try {
+        await fetch(`https://blog-api-assignment.up.railway.app/posts/${punId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+        // Remove the pun from the DOM
+        document.querySelector(`[data-id="${punId}"]`).remove();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function updatePun(punId, newData) {
+    try {
+        await fetch(`https://blog-api-assignment.up.railway.app/posts/${punId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newData)
+        });
+        // Reload the page or update the data in the DOM as needed
+        // For simplicity, reloading the page here
+        location.reload();
+    } catch (error) {
+        console.error(error);
     }
 }
