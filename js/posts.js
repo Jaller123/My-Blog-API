6e0cdec3606cacf2ec74e340ad344db731c8b693
Kenatho <89846381+Kenatho@users.noreply.github.com>
2023-12-13 12:35:53 +0100

    Fixed ID Link href

--- js/posts.js
+++ js/posts.js
@@ -5,10 +5,11 @@ async function fetchAllPuns() {
         const response = await fetch('https://blog-api-assignment.up.railway.app/posts');
         const puns = await response.json();
 
+        //Loops through all the data
         let punsListHTML = "";
         for (let pun of puns) {
             let punDate = new Date(pun.date);
-
+            //Applies the data in the HTML
             punsListHTML += `
                 <tr data-id="${pun._id}">
                     <h2 class="editable" contenteditable="true">${pun.title}</h2>
@@ -17,9 +18,12 @@ async function fetchAllPuns() {
                     
                     <td><p>${pun.tags}</p></td>
                     <p>${pun.content.slice(0, 100)}</p>
-                    <a href="post.html">Read more</a>
+                    <a href="post.html?id=${pun._id}">Read more</a>
                 </tr>
             `;
+            //Slice limits the cahracters to 100 only.
+            /*the link href to ${pun._id} refers to sending data from this
+            page to the post.js*/
         }
         document.getElementById('pun-list-body').innerHTML = punsListHTML;
     } catch (error) {
